310a79ae4b369f29412391eb7ed8e253da8fb1b8
malli24 <malleswari82006@gmail.com>
2024-12-07 12:34:08 +0530

    Add files via upload

---
+++ countinue.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Countinue
+{
+    public static void main(String[]args)
+    {
+        System.out.println("continue");
+        for(int i=5;i<5;i++)
+        {
+            if(i==2)
+            continue;
+            System.out.println(i);
+        }
+
+
+
+    }
+}
