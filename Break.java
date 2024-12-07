dd4101fa4721b86ab2307133747792ad93bd6958
malli24 <malleswari82006@gmail.com>
2024-12-07 12:28:44 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Break
+{
+    public static void main(String[]args)
+    {
+        System.out.println("break");
+        for(int i=5;i<5;i++)
+        {
+            if(i==2)
+            break;
+            System.out.println(i);
+        }
+
+
+
+    }
+}
